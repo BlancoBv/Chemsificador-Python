4e3e5c0e19969de8234c45c6ca9f71f2741b78c3
franc <blanco.email>
2022-11-05 10:17:27 -0600

    mejorado el algoritmo 0.0.1

--- GUI_prueba.py
+++ GUI_prueba.py
@@ -1,24 +1,36 @@
 import tkinter as tk
-from chemsificar import main as Start
+from chemsificar import main as Chemsificar
 
 def VentanaPrincipal(): 
   def TextoCaja():
     texto= cajaTexto .get() +" "
-    Start(texto)
+    #Feedback al usuario si no ingresa ningun dato
+    if texto== " ":
+      cajaResultado["text"]="Escribe algo"
+    else:
+     cajaResultado["text"]=Chemsificar(texto)
+     
   #contenedor principal
   ventana= tk.Tk()
   ventana. geometry("300x300")
-  #contenedortexto
-  cajaTexto= tk.Entry(ventana)
+  ventana.title("Chemsificador")
+  #contenedo de texto de entrada
+  cajaTexto= tk.Entry(ventana, font="Consolas")
   cajaTexto.pack()
   #boton de envio
   botonEnviar= tk.Button(ventana, text="Chemsificar texto", command=TextoCaja)
   botonEnviar.pack()
+  #contenedor de texto de salida
+  cajaResultado= tk.Label(ventana, text="Chemfisicador owo", background="white", width="30", relief="raised", height="10")
+  cajaResultado.pack()
+  #boton para copiar texto al portapapeles
+  botonCopiar= tk.Button(ventana,text="Copiar al portapapeles")
+  botonCopiar.pack()
   return ventana
 
 def main():
   #salida de todo
   VentanaPrincipal().mainloop()
-  
+#Ejecuta funcion principal, no borrar
 if __name__== "__main__":
   main()
--- chemsificar.py
+++ chemsificar.py
@@ -3,6 +3,7 @@
 #Convierte el input en un array
 def Convertirarray(x):
   return [c for c in x]
+  
 #compara cada uno de los elementos de la cadena
 def Comparacion(texto):
   #variables
@@ -10,6 +11,13 @@ def Comparacion(texto):
   letraAnterior= ""
   nuevaCadena=[]
   i= 0
+
+  #funcion que calcula el valor de E
+  def E():
+    valueE= len(texto)-2
+    print("Valor de E:",valueE)
+    return valueE
+  
 #ciclo de comparacion
   while i <len(texto)-1:
     letraAnterior= texto[i]
@@ -17,20 +25,42 @@ def Comparacion(texto):
       nuevaCadena.append(letraAnterior)
       nuevaCadena.append("m")
       nuevaCadena.append(texto[i+1])
-      i+=2
+      #i+=2
+      i+=1
     else:
       nuevaCadena.append(letraAnterior)
       nuevaCadena.append(texto[i+1])
-      i+=2
-  return nuevaCadena
+      #i+=2
+      i+=1
+  print("cadena resultante:",nuevaCadena)
+  return nuevaCadena, E()
+
 #devuelve la cadena final
 def ListaTexto(final):
   out= "".join([str(item) for item in final])
   print("Texto chemsificado:",out, sep="\n")
-
+  return out
+#funcion que elimina duplicados
+def LimpiarCadena(cadenaResult):
+  #variables
+  e=cadenaResult[1]
+  contadorDel=0
+  cadenaAsign= cadenaResult[0]
+  
+  for i in range(len(cadenaAsign)-1):
+    if cadenaAsign[i]==cadenaAsign[i+1]:
+      cadenaAsign.pop(i)
+      contadorDel+=1
+      if contadorDel == e:
+        #detiene el ciclo para evitar errores
+        break
+  return cadenaAsign
+  
 #Llamado de funciones aqui
 def main(cadena):
-  ListaTexto(Comparacion(Convertirarray(cadena)))
+  #retorna el resultado para ser usado en la GUI
+  ola=Comparacion(Convertirarray(cadena)) #ola es una tupla de dos valores accesar con indices 0,1
+  return ListaTexto(LimpiarCadena(ola))
  #Ejecuta funcion principal, no borrar
 if __name__=="__main__":
   #cadena= input("Ingresa el texto a chemsificar: ") + " "
